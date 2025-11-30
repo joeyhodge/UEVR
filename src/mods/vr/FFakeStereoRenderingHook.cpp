42af3d890a11fe7e8c638cffd03f82e1677705c1
joeyhodge <96703964+joeyhodge@users.noreply.github.com>
2025-11-29 22:43:32 -0500

    Fix UE5 splitscreen matrix casts

--- src/mods/vr/FFakeStereoRenderingHook.cpp
+++ src/mods/vr/FFakeStereoRenderingHook.cpp
@@ -2981,7 +2981,7 @@ sdk::FSceneView* FFakeStereoRenderingHook::sceneview_constructor(sdk::FSceneView
 
             euler = euler_d;
 
-            const auto view_rot_mat = conversion_mat * utility::math::ue_inverse_rotation_matrix(euler);
+            const auto view_rot_mat = conversion_mat * glm::dmat4{utility::math::ue_inverse_rotation_matrix(euler)};
 
             FIntRect updated_view_rect{x, y, x + w, y + h};
             *(FIntRect*)&options.view_rect = updated_view_rect;
@@ -3142,7 +3142,7 @@ sdk::FSceneView* FFakeStereoRenderingHook::sceneview_constructor(sdk::FSceneView
 
             euler = euler_d;
 
-            const auto view_rot_mat = conversion_mat * utility::math::ue_inverse_rotation_matrix(euler);
+            const auto view_rot_mat = conversion_mat * glm::dmat4{utility::math::ue_inverse_rotation_matrix(euler)};
 
             FIntRect updated_view_rect{vr->get_hmd_width(), 0, vr->get_hmd_width() * 2, vr->get_hmd_height()};
 
