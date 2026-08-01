198b735912d716882897a1e0bd394257e74f9117
joeyhodge <96703964+joeyhodge@users.noreply.github.com>
2026-08-01 12:39:04 -0400

    Fix Dead Island 2 direct Synced eye startup

--- src/mods/vr/D3D12Component.hpp
+++ src/mods/vr/D3D12Component.hpp
@@ -407,6 +407,7 @@ private:
     uint32_t m_last_rendered_frame{0};
     bool m_force_reset{true};
     bool m_last_afr_state{false};
+    bool m_dead_island_2_synced_eye_rebase_pending{};
     bool m_submitted_left_eye{false};
     uint64_t m_swapchain_recreate_count{};
     uint32_t m_last_swapchain_recreate_reasons{};
