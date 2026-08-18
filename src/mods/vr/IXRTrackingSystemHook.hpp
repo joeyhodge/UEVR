357f40169da16374fe11a81ed4e2dfc1c355d2ac
joeyhodge <96703964+joeyhodge@users.noreply.github.com>
2026-08-17 22:38:47 -0400

    Fix Days Gone HMD and controller aim

--- src/mods/vr/IXRTrackingSystemHook.hpp
+++ src/mods/vr/IXRTrackingSystemHook.hpp
@@ -2,6 +2,8 @@
 
 #include <cstdint>
 #include <array>
+#include <atomic>
+#include <chrono>
 #include <memory>
 
 #include <sdk/StereoStuff.hpp>
@@ -12,6 +14,9 @@
 namespace sdk {
 class UEngine;
 class UGameEngine;
+class UClass;
+class UFunction;
+class UObject;
 class IXRTrackingSystem;
 class IXRCamera;
 class IHeadMountedDisplay;
@@ -100,6 +105,10 @@ private:
 
     void pre_update_view_rotation(sdk::UObject* reference_obj, Rotator<float>* rot);
 
+    void update_daysgone_manual_aim_trace(sdk::UGameEngine* engine);
+    bool try_install_daysgone_manual_aim_trace(sdk::UObject* manual_aim);
+    static void* daysgone_six_axis_aim_trace(void* manual_aim, float* output_pair);
+
     FFakeStereoRenderingHook* m_stereo_hook{nullptr};
 
     struct TrackingSystem {
@@ -148,6 +157,19 @@ private:
     uintptr_t m_addr_of_process_view_rotation_ptr{};
     //std::unique_ptr<PointerHook> m_process_view_rotation_hook{};
     safetyhook::InlineHook m_process_view_rotation_hook{};
+    safetyhook::InlineHook m_daysgone_six_axis_aim_hook{};
+    std::atomic<sdk::UObject*> m_daysgone_active_manual_aim{};
+    std::atomic<float> m_daysgone_desired_pitch_offset_degrees{};
+    std::atomic<float> m_daysgone_desired_yaw_offset_degrees{};
+    std::atomic<uint64_t> m_daysgone_desired_aim_sequence{};
+    std::atomic<uint64_t> m_daysgone_consumed_aim_sequence{};
+    std::atomic<uint64_t> m_daysgone_desired_aim_sample_time_ms{};
+    std::atomic<uint64_t> m_daysgone_aim_trace_next_log_ms{};
+    sdk::UClass* m_daysgone_manual_aim_class{};
+    uintptr_t m_daysgone_manual_aim_update{};
+    uintptr_t m_daysgone_six_axis_decoder{};
+    uintptr_t m_daysgone_rejected_manual_aim_vtable{};
+    std::chrono::steady_clock::time_point m_daysgone_manual_aim_next_retry{};
     bool m_attempted_hook_view_rotation{false};
     bool m_initialized{false};
     bool m_is_leq_4_25{false}; // <= 4.25, IsHeadTrackingAllowedForWorld does not exist
